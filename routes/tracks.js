a94f88f88f9fcea070249a10b2cdc9226e5564ea
cpribaz <cpribaz@uwo.ca>
2022-12-07 01:09:42 -0500

    [Lab 4] - CP: fixed buttons

--- routes/tracks.js
+++ routes/tracks.js
@@ -11,11 +11,11 @@ router.get('/', async (req, res) =>{
     if(req.query.track_title != null && req.query.track_title != '') {
         query = query.regex('track_title', new RegExp(req.query.track_title, 'i'))
     }
-    if(req.query.publishedBefore != null && req.query.publishedBefore != '') {
-        query = query.lte('createdAt', req.query.publishedBefore)
+    if(req.query.artist_name != null && req.query.artist_name != '') {
+        query = query.regex('artist_name', req.query.artist_name)
     }
-    if(req.query.publishedAfter != null && req.query.publishedAfter != '') {
-        query = query.gte('createdAt', req.query.publishedAfter)
+    if(req.query.track_genres != null && req.query.track_genres != '') {
+        query = query.gte('track_genres', req.query.track_genres)
     }
     try{
         const tracks = await query.limit(20).exec();
